9d5886d3a5ccd8ff3ce45990194f843c0ca85a73
AbeTavarez <abeincwireless@gmail.com>
2024-02-29 13:33:14 -0500

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,30 @@
+import Link from "next/link";
 import { db } from "@/db";
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
 
   return (
-  <div>
-    <h1>Home Page</h1>
-
-    {snippets.map((snippet) => (
-      <div key={snippet.id}>
-        {snippet.title}
+    <div>
+      <div className='flex m-2 justify-between items-center'>
+        <h1 className='text-xl bold'>Snippets</h1>
+        <Link href={"/snippets/new"} className='p-2 border rounded'>
+          New
+        </Link>
       </div>
-    ))}
-  </div> 
+
+      {snippets.map((snippet) => (
+        <div className='flex flex-col gap-3'>
+          <Link
+            href={`/snippets/${snippet.id}`}
+            key={snippet.id}
+            className='flex justify-between items-center p-2 border rounded'
+          >
+            <div>{snippet.title}</div>
+            <div>View</div>
+          </Link>
+        </div>
+      ))}
+    </div>
   );
 }
