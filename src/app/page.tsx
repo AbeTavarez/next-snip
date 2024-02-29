9c32804da7f3467e85b1247536b5db82e2262649
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-02-28 23:06:48 -0500

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import { db } from "@/db";
 
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
 
-export default function Home() {
   return (
   <div>
     <h1>Home Page</h1>
+
+    {snippets.map((snippet) => (
+      <div key={snippet.id}>
+        {snippet.title}
+      </div>
+    ))}
   </div> 
   );
 }
