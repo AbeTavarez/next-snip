1628bfd08db3d8922bea3d4a82e9e27a2f7c5276
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-03-18 14:59:04 -0400

    create snippet action now uses useFormState Hook for data validation

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,28 +1,14 @@
-import { redirect } from "next/navigation";
-import { db } from "@/db";
+"use client";
+import { useFormState } from "react-dom";
+import * as actions from "@/actions";
 
 export default function SnipperCreatePage() {
-  const createSnippet = async (formData: FormData) => {
-    "use server"; // server action
-
-    // form values
-    const title = formData.get("title") as string;
-    const code = formData.get("code") as string;
-
-    // create record
-    const snippet = await db.snippet.create({
-      data: {
-        title,
-        code,
-      },
-    });
-
-    console.log(snippet);
-    redirect('/');
-  };
+  const [formState, action] = useFormState(actions.createSnippet, {
+    message: "",
+  });
 
   return (
-    <form action={createSnippet}>
+    <form action={action}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
 
       <div className="flex flex-col gap-4">
@@ -47,6 +33,8 @@ export default function SnipperCreatePage() {
             className="border rounded p-2 w-full"
           />
         </div>
+
+        { formState.message ? <div className="my-2 p-2 bg-red-200 border rounded border-red-400">{formState.message}</div> : null }
 
         <button type="submit" className="rounded p-2 bg-blue-200">
           Create
